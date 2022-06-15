99e1af8998e96604c01d16e56f30a1f1c97aec7e
Délita Makanda <delita.makanda@gmail.com>
2022-06-15 20:59:34 +0200

    refactor routes

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,10 @@ import {
   AccountBookOutlined
 } from '@ant-design/icons'
 import {
-  Routes,
-  Route,
   Outlet,
   useNavigate
 } from 'react-router-dom'
 const { Header, Footer, Content } = Layout
-
-const Dashboard = React.lazy(() => import('./pages/Dashboard'))
-const Signup = React.lazy(() => import('./pages/Signup'))
-const Login = React.lazy(() => import('./pages/Login'))
-const Notification = React.lazy(() => import('./pages/Notification'))
-const Message = React.lazy(() => import('./pages/Message'))
-const Offer = React.lazy(() => import('./pages/Offer'))
-import NoMatch from './pages/NoMatch'
 
 const BaseLayout = () => {
   const navigate = useNavigate()
@@ -121,53 +111,9 @@ const BaseLayout = () => {
 
 const App = () => {
     return (
-      <Routes>
-        <Route path='/' element={<BaseLayout />}>
-          <Route index element={
-            <React.Suspense fallback={<>...</>}>
-              <Offer />
-            </React.Suspense>
-          } />
-          <Route
-            path='messages/*'
-            element={
-              <React.Suspense fallback={<>...</>}>
-                <Message />
-              </React.Suspense>
-            } 
-          />
-          <Route
-            path='notifications/*'
-            element={
-              <React.Suspense fallback={<>...</>}>
-                <Notification />
-              </React.Suspense>
-            } 
-          />
-          <Route
-            path='dashboard/*'
-            element={
-              <React.Suspense fallback={<>...</>}>
-                <Dashboard />
-              </React.Suspense>
-            } />
-          <Route
-            path='login/*'
-            element={
-              <React.Suspense fallback={<>...</>}>
-                <Login />
-              </React.Suspense>
-            } />
-          <Route
-            path='signup/*'
-            element={
-              <React.Suspense fallback={<>...</>}>
-                <Signup />
-              </React.Suspense>
-            } />
-          <Route path='*' element={<NoMatch />} />
-        </Route>
-      </Routes>
+      <div>
+        <BaseLayout />
+      </div>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,65 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import 'antd/dist/antd.css'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+const Dashboard = React.lazy(() => import('./pages/Dashboard'))
+const Signup = React.lazy(() => import('./pages/Signup'))
+const Login = React.lazy(() => import('./pages/Login'))
+const Notification = React.lazy(() => import('./pages/Notification'))
+const Message = React.lazy(() => import('./pages/Message'))
+const Offer = React.lazy(() => import('./pages/Offer'))
+import NoMatch from './pages/NoMatch'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path='/' element={<App />}>
+        <Route index element={
+            <React.Suspense fallback={<>...</>}>
+              <Offer />
+            </React.Suspense>
+          } />
+          <Route
+            path='messages/*'
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <Message />
+              </React.Suspense>
+            } 
+          />
+          <Route
+            path='notifications/*'
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <Notification />
+              </React.Suspense>
+            } 
+          />
+          <Route
+            path='dashboard/*'
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <Dashboard />
+              </React.Suspense>
+            } />
+          <Route
+            path='login/*'
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <Login />
+              </React.Suspense>
+            } />
+          <Route
+            path='signup/*'
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <Signup />
+              </React.Suspense>
+            } />
+          <Route path='*' element={<NoMatch />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
